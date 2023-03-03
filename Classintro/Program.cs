371ccea406e1e4e4a8d1698c2887e5276ecd9917
Tech <Tech@DESKTOP-JPHC14P>
2023-03-04 01:46:14 +0300

    Add project files.

---
+++ Classintro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Classintro
+{
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Eğitmen { get; set; }
+        public int İzlenmeOranı { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Eğitmen = "Melih";
+            kurs1.İzlenmeOranı = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Eğitmen = "Melih Mert Sakur";
+            kurs2.İzlenmeOranı = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Eğitmen = "Melih Mert";
+            kurs3.İzlenmeOranı = 80;
+
+            //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Eğitmen);
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Kurs kurs = kurslar[i];
+                Console.WriteLine(kurs.KursAdi);
+                Console.WriteLine(kurs.Eğitmen);
+                Console.WriteLine(kurs.İzlenmeOranı);
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Döngüler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "yazılım geliştirme kampı";
+            string kurs2 = "programlamaya başlangıç temel kurs";
+            string kurs3 = "java";
+            //array dizi
+            string[] kurslar = new string[] { "yazılım geliştirme kampı", "programlamaya başlangıç temel kurs", "java" };
+            for (int i = 1; i <=10 ; i++)
+            {
+                Console.WriteLine(i); 
+            }
+        }
+    }
+}
